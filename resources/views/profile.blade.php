1eb29423b12a5933f80b2d30ae399dfb8da88843
nelaa7 <nelaanjani22@gmail.com>
2022-10-10 20:21:35 +0700

    profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -113,29 +113,26 @@
         </div>
     </header>
     <!-- header-end -->
-
+    <br>
     <div class="container mt-4 mb-4 p-3 d-flex justify-content-center"> 
         <div class="card p-4"> 
             <div class=" image d-flex flex-column justify-content-center align-items-center"> 
                 <button class="btn btn-secondary"> 
-                    <img src="https://i.imgur.com/wvxPV9S.png" height="100" width="100" /></button> 
-                    <span class="name mt-3">Eleanor Pena</span> <span class="idd">@eleanorpena</span> 
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" height="100" width="100" />
+                </button> 
+                    <span class="name mt-3">Eleanor Pena</span> 
+                    <p class="idd">eleanorpena@gmail.com</p> 
                     <div class="d-flex flex-row justify-content-center align-items-center gap-2"> 
-                        <span class="idd1">Oxc4c16a645_b21a</span> <span><i class="fa fa-copy"></i></span> 
+                        <span class="idd1"></span> 
                     </div> <div class="d-flex flex-row justify-content-center align-items-center mt-3"> 
-                        <span class="number">1069 <span class="follow">Followers</span>
-                    </span> </div> <div class=" d-flex mt-2"> 
+                        
+                     </div> <div class=" d-flex mt-2"> 
                         <button class="btn1 btn-dark">Edit Profile</button> </div>
                          <div class="text mt-3"> 
-                            <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br>
-                             Artist/ Creative Director by Day #NFT minting@ with FND night. </span> 
+                            <p>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br>
+                             Artist/ Creative Director by Day #NFT minting@ with FND night. </p> 
                             </div> <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> 
-                                <span><i class="fa fa-twitter"></i></span>
-                                 <span><i class="fa fa-facebook-f"></i></span> 
-                                 <span><i class="fa fa-instagram"></i></span>
-                                  <span><i class="fa fa-linkedin"></i></span> 
-                                </div> <div class=" px-2 rounded mt-4 date "> 
-                                    <span class="join">Joined May,2021</span> </div> </div> </div>
+                               
 </div>
 
 
