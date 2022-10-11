9fdad20d660f1ebc89d0aa2e4d50cb61c745179d
nelaa7 <nelaanjani22@gmail.com>
2022-10-11 11:35:37 +0700

    blog

---
+++ app/Http/Controllers/DonorController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DonorController extends Controller{
+    
+    public function login(){
+        return view ('login');
+    }
+
+    public function index(){
+        return view ('index');
+    }
+
+    public function blog(){
+        return view ('blog');
+    }
+
+    public function single-blog1(){
+        return view ('single-blog1');
+    }
+
+
+    
+}
