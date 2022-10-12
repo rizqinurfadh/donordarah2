b6b6948752a642d2cd7c6ad5fa98fcf906a1392d
nelaa7 <nelaanjani22@gmail.com>
2022-10-12 12:15:15 +0700

    admin

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts\fonts-login\icomoon\style.css">
+
+    <link rel="stylesheet" href="css\css-login\owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css\css-login\bootstrap.min.css">
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="css\css-login\style.css">
+
+    <title>Login</title>
+  </head>
+  <body>
+  
+
+  
+  <div class="content">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 order-md-2">
+          <img src="img\img-login\login2.png" alt="Image" class="img-fluid">
+        </div>
+        <div class="col-md-6 contents">
+          <div class="row justify-content-center">
+            <div class="col-md-8">
+              <div class="mb-4">
+              <h3>Sign In to <strong>Blutspender</strong></h3>
+              <p class="mb-4"> Bagi siapapun yang sedang membutuhkan,
+                            darah menjadi kebutuhan paling berharga</p>
+            </div>
+            <form action="#" method="post">
+              <div class="form-group first">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username">
+
+              </div>
+              <div class="form-group last mb-4">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password">
+                
+              </div>
+              
+              <div class="d-flex mb-5 align-items-center ">
+                <label class="control control--checkbox mb-0 "><span class="caption">Remember me</span>
+                  <input type="checkbox" checked="checked"/>
+                  <div class="control__indicator"></div>
+                </label>
+                <span class="ml-auto"><a href="forgot-password" class="forgot-password">Forgot Password</a></span> 
+              </div>
+
+              <input type="submit"  value="Log In" class="btn text-white btn-block btn-danger">
+
+              <span class="d-block text-left my-4 text-muted"> or sign in with</span>
+              
+              <div class="social-login">
+                <a href="#" class="facebook">
+                  <span class="icon-facebook mr-3"></span> 
+                </a>
+                <a href="#" class="twitter">
+                  <span class="icon-twitter mr-3"></span> 
+                </a>
+                <a href="#" class="google">
+                  <span class="icon-google mr-3"></span> 
+                </a>
+              </div>
+            </form>
+            </div>
+          </div>
+          
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+  
+    <script src="js/js-login/jquery-3.3.1.min.js"></script>
+    <script src="js/js-login/popper.min.js"></script>
+    <script src="js/js-login/bootstrap.min.js"></script>
+    <script src="js/js-login/main.js"></script>
+  </body>
+</html>
